a8ea7f0f275c733b071c153bbedde6d665f89b80
juandiego1023 <juandiegoariasmartin@gmail.com>
2014-12-05 10:28:26 +0100

    new

--- jMySql/src/serpis/ad/MySql.java
+++ jMySql/src/serpis/ad/MySql.java
@@ -5,26 +5,40 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.*;
+
+import java.sql.PreparedStatement;
 
 public class MySql {
+	
+	private static Scanner ScarletJohanson=new Scanner(System.in);
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		System.out.println("Hola");
+		System.out.println("Menu:0-salir/1-nuevo/2-editar/3-eliminar/4-ver");
+		opcion  		
 		//Class.forName("com.mysql.jdbc.Driver");
 		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost/dbprueba?user=root&password=sistemas");
-		Statement statement=connection.createStatement();
-		ResultSet resultset=statement.executeQuery("select * from categoria");
+		//Statement statement=connection.createStatement();
+		//Sin parametros
+		//ResultSet resultset=statement.executeQuery("select * from categoria");
 		
-		while(resultset.next()){
-			System.out.printf("id=%4s nombre=%s\n",resultset.getObject("id"),resultset.getObject("nombre"));
+		//Con parametros
+		PreparedStatement preparedStatement=connection.prepareStatement("select * from categoria where id=?"); 
+		preparedStatement.setLong(1,19);//en el primer interrogante de id busca el 19
+		ResultSet resultSet=preparedStatement.executeQuery();
+		
+		while(resultSet.next()){
+			System.out.printf("id=%4s nombre=%s\n",resultSet.getObject("id"),resultSet.getObject("nombre"));
+			resultSet.getObject("nombre");
 		}
 		
 		
-		resultset.close();
+		resultSet.close();
 		connection.close();
 				
 				
 	}
 
 }
+m
